1e510f81b6894d7ce2f981ba976a39edf68d2ba1
xxoellie <elliesohyeon1202@gmail.com>
2021-08-06 03:25:46 +0900

    8/6 django self study commit 10

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,4 +21,5 @@ def hello_world(request):
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else :
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
