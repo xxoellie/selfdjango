8337020a55579151321bb5c3aedc7b7a52e9def4
xxoellie <elliesohyeon1202@gmail.com>
2021-08-12 16:12:52 +0900

    8/12 django self study commit 7

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+
+from projectapp.forms import ProjectCreationForm
+from projectapp.models import Project
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'projectapp/create.html'
+
+class ProjectDetailView(DetailView):
+    model = Project
+    form_class = ProjectCreationForm
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
