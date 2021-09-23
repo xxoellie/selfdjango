33c30721ace8c9a0767c25bc91501891fd5b2fbb
xxoellie <elliesohyeon1202@gmail.com>
2021-09-23 15:47:11 +0900

    9/23 django self study commit 1

--- gis_1ban_project/settings/deploy.py
+++ gis_1ban_project/settings/deploy.py
@@ -14,8 +14,15 @@ while True :
     value = line[start+1:]
     env_list[key] = value
 
+def read_secrets(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secrets('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -29,8 +36,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secrets('MARIADB_USER'),
+        'PASSWORD': read_secrets('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
